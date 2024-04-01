e4b5f468bef66e7228ccf9cc1c0bec15c8912476
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-04-01 13:24:40 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1697,7 +1697,7 @@ const renderPage = async (env) => {
 	</head>
 	
 	<body>
-		<h1 style="text-align: center; color: #2980b9">BPB Panel <span style="font-size: smaller;">2.2</span> 🪄</h1>
+		<h1 style="text-align: center; color: #2980b9">BPB Panel <span style="font-size: smaller;">2.2</span> ❄️☃️</h1>
 		<div class="form-container">
             <h2>FRAGMENT SETTINGS ⚙️</h2>
 			<form id="configForm">
@@ -2129,7 +2129,7 @@ const renderLoginPage = async () => {
     </head>
     <body>
         <div class="container">
-            <h1>BPB Panel <span style="font-size: smaller;">2.2</span> 🪄 🔐</h1>
+            <h1>BPB Panel <span style="font-size: smaller;">2.2</span> ⛄️</h1>
             <div class="form-container">
                 <h2>User Login</h2>
                 <form id="loginForm">
