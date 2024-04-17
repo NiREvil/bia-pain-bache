4283b186473082ee35fe850cc1649275b6da5f52
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-04-17 23:40:43 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let userID = 'cf8cf683-40fa-4cd3-93cd-820071b11c90';
 
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records
 
-const proxyIPs= ['23.90.144.167'];// OR USE const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= ['cdn-all.xn--b6gac.eu.org'];// OR USE const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
