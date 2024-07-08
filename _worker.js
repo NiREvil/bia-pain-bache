d4076af6106cfd1d0cb6235476bb1f0620bb56fd
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-07-08 17:25:02 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2120,7 +2120,7 @@ const renderHomePage = async (env, hostName, fragConfigs) => {
                 </div>
 				<div id="apply" class="form-control">
 					<div style="grid-column: 2; width: 100%;">
-						<input type="submit" id="applyButton" class="button disabled" value=":trollface: APPLY SETTINGS :trollface:" form="configForm">
+						<input type="submit" id="applyButton" class="button disabled" value="🪩 APPLY SETTINGS 🪩" form="configForm">
 					</div>
 				</div>
 			</form>
